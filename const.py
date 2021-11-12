5a8b7678aba8d5b6f33a36276100899184bb3120
Hippan <hipp@email.com>
2021-11-12 22:53:04 +0300

    Change point at pictures

--- const.py
+++ const.py
@@ -1,6 +1,6 @@
 SUITS = ['heart', 'deamonds', 'club', 'spade']
 tmp = [str(x) for x in range(2, 11)] + ['jack', 'queen', 'king', 'ace']
-POINTS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 11]
+POINTS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 RANKS = {}
 for i in range(len(tmp)):
     RANKS[tmp[i]] = POINTS[i]
